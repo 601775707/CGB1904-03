704041e229f5405218bf9f25460b070086848e17
Think <Think@DESKTOP-SETMM4P.mshome.net>
2019-07-18 15:19:54 +0800

    测试推送：新增礼品

---
+++ src/main/java/com/cy/dao/GiftDao.java
@@ -0,0 +1,8 @@
+package com.cy.dao;
+
+import com.cy.entity.Gift;
+
+public interface GiftDao {
+
+	int insertObejct(Gift entity);
+}
---
+++ src/main/java/com/cy/service/GiftService.java
@@ -0,0 +1,5 @@
+package com.cy.service;
+
+public interface GiftService {
+
+}
