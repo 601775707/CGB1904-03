1a3e65ce3820183975b35a9f43cb5a8d536ac996
601775707 <601775707@qq.com>
2019-07-18 15:18:36 +0800

    新建类
    Member实体对象
    会员状态枚举

---
+++ src/main/java/com/cy/dao/Member.java
@@ -0,0 +1,42 @@
+package com.cy.dao;
+
+import java.util.Date;
+
+import com.cy.vo.MemberState;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * member实体类
+ */
+@Data
+@ToString
+public class Member {
+	// 主键id
+	private Integer id;
+	// 余额
+	private double balance;
+	// 生日
+	private Date birthday;
+	// 积分
+	private double memberIntegral;
+	// 会员名
+	private String memberName;
+	// 密码
+	private String password;
+	// 盐值
+	private String salt;
+	// 手机
+	private String phone;
+	// 性别
+	private String sex;
+	// 会员状态,枚举
+	private MemberState state;
+	// 会员等级
+	private Integer memberGradeId;
+	// 邮箱
+	private String email;
+	// 头像图片url
+	private String iconPath;
+}
---
+++ src/main/java/com/cy/vo/MemberState.java
@@ -0,0 +1,8 @@
+package com.cy.vo;
+
+/**
+ * 用户状态枚举类
+ */
+public enum MemberState {
+	停用,挂失,正常
+}
