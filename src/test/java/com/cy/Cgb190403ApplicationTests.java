f844c13bfe72547c2803f92cffe6d30aabf38cb1
601775707 <601775707@qq.com>
2019-07-19 16:55:37 +0800

    更新

--- src/test/java/com/cy/Cgb190403ApplicationTests.java
+++ src/test/java/com/cy/Cgb190403ApplicationTests.java
@@ -8,13 +8,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.cy.controller.MemberController;
 import com.cy.dao.GoodsDao;
+import com.cy.dao.MemberDao;
 import com.cy.entity.Goods;
+import com.cy.entity.Member;
+import com.cy.service.MemberService;
+import com.github.pagehelper.PageInfo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Cgb190403ApplicationTests {
 
+	@Autowired
+	private MemberController memberController;
+	@Autowired
+	private MemberService memberService;
+	@Autowired
+	private MemberDao memberDao;
 	@Test
 	public void contextLoads() {
 	}
@@ -24,7 +35,6 @@ public class Cgb190403ApplicationTests {
 	
 	@Test
 	public void testFindGoodsList() {
-		
 		List<Goods> list = goodsDao.selectAll();
 		
 		for (Goods goods : list) {
