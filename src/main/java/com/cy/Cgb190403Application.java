8911e8d66746965fd0b013a92a983328615d6b19
601775707 <601775707@qq.com>
2019-07-19 16:55:49 +0800

    更新

--- src/main/java/com/cy/Cgb190403Application.java
+++ src/main/java/com/cy/Cgb190403Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("com.cy.dao")
+@MapperScan(basePackages = "com.cy.dao")
 public class Cgb190403Application {
 
 	public static void main(String[] args) {
